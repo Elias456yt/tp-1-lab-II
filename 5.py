ea8d3ea25c3071607a6db6c332858c8761bf18f2
Elias456yt <166611763+Elias456yt@users.noreply.github.com>
2024-04-17 11:16:23 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,7 @@
+#5
+datos_personales = {"nombre": "Elias", "edad" : 25, "ciudad" : "ciudad de Argentina"}
+print(datos_personales["nombre"])
+
+datos_personales["edad"] = 30
+datos_personales["genero"] = "masculino"
+print(datos_personales)
