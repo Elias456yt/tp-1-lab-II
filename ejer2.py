a958a24a7a807c13e9bf89bf00ececc6104c7473
Elias456yt <166611763+Elias456yt@users.noreply.github.com>
2024-04-17 11:14:32 -0300

    Create ejer2.py

---
+++ ejer2.py
@@ -0,0 +1,16 @@
+#act2
+a = 10
+b = 2 
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b
+exponente = a ** b
+
+print("suma" , suma)
+print("resta" , resta)
+print("multiplicacion" , multiplicacion)
+print("division" , division)
+print("modulo" , modulo)
+print("exponente" , exponente)
