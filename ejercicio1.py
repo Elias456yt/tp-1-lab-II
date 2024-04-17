88c773a07156a3ac244170e2ae15c3f085e7feb9
Elias456yt <166611763+Elias456yt@users.noreply.github.com>
2024-04-17 11:13:23 -0300

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#act 1
+#variables
+
+numero_entero = 7
+numero_flotante = 2.5
+cadena = "`hay q vivir, hay q llorar la vida es una y es un carnaval`"
+booleano = True
+
+# imprimir valores
+
+print("numero entero" , numero_entero)
+print("numero flotante" , numero_flotante)
+print("cadena:" , cadena)
+print("es veradero?" , booleano)
