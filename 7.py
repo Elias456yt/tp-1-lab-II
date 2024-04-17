2b9721fd4f70d6f03cf8ada80e7c72059a73c63f
Elias456yt <166611763+Elias456yt@users.noreply.github.com>
2024-04-17 11:17:23 -0300

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,9 @@
+# 7
+import math
+def calcular_area_circulo(radio):
+  area = math.pi * radio**2
+  return area
+
+print(calcular_area_circulo(3))
+print(calcular_area_circulo (5))
+print(calcular_area_circulo (1.5))
